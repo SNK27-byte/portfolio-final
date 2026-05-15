294c72ee0864873c53f0dfa4e24bbed179653c82
Paul <warmerise2015@gmail.com>
2026-05-15 14:41:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# installation de node
+
+
+prendre la version LTS
+
+https://nodejs.org/en/download
+
+ne rien cocher sauf confid
+
+## installation de sass
+
+ouvrir un terminal
+
+npm install -g sass
+
+si message: npm : Impossible de charger le fichier C:\Program Files\nodejs\npm.ps1, car l’exécution de scripts est désactivée sur
+ce système. Pour plus d’informations, consultez about_Execution_Policies à l’adresse
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+## démarrer sass pour scss
+
+sass ./assets/style.scss .build/style.css --style=compressed --watch
+
---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/bootstrap-5.3.8-dist/css/bootstrap.min.css">
+    <script src="assets/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="build/style.css">
+    <title>Portfolio</title>
+</head>
+<body>
+        <div class="container">
+
+          <!-- Top Tab -->
+          <div class="top-bar">
+            <div class="tab">
+              <div class="tab-left">
+                <div class="tab-logo">LP</div>
+                <span>HOME</span>
+              </div>
+              <span class="close">×</span>
+            </div>
+          </div>
+
+          <!-- Top Search -->
+          <div class="top-search">
+            <div class="top-search-bar">
+              Rechercher avec google ou saisir une adresse
+            </div>
+          </div>
+
+          <!-- Logo -->
+          <div class="logo-container">
+            <img src="LogoLP.png" alt="LP Logo" />
+          </div>
+
+          <!-- Main Search -->
+          <div class="main-search">
+            <div class="main-search-bar">
+              <div class="search-icon">⌘</div>
+              Rechercher avec google ou autre
+            </div>
+          </div>
+
+          <!-- Apps -->
+          <div class="apps-grid">
+            <div class="app">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg" alt="Photoshop">
+            </div>
+
+            <div class="app">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/f/fb/Adobe_Illustrator_CC_icon.svg" alt="Illustrator">
+            </div>
+
+            <div class="app">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Adobe_InDesign_CC_icon.svg" alt="InDesign">
+            </div>
+
+            <div class="app">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg" alt="VS Code">
+            </div>
+        </div>
+</body>
+</html>
