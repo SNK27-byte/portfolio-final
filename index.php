334ff988868d9aa3f11995b01a8253dbdc23dabd
Paul <warmerise2015@gmail.com>
2026-05-15 15:54:57 +0200

    Modif site accueil

--- index.php
+++ index.php
@@ -5,47 +5,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/bootstrap-5.3.8-dist/css/bootstrap.min.css">
-    <script src="assets/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="build/style.css">
     <title>Portfolio</title>
 </head>
 <body>
         <div class="container">
 
-          <!-- Top Tab -->
-          <div class="top-bar">
-            <div class="tab">
-              <div class="tab-left">
-                <div class="tab-logo">LP</div>
-                <span>HOME</span>
-              </div>
-              <span class="close">×</span>
-            </div>
-          </div>
-
-          <!-- Top Search -->
-          <div class="top-search">
-            <div class="top-search-bar">
-              Rechercher avec google ou saisir une adresse
-            </div>
-          </div>
-
-          <!-- Logo -->
           <div class="logo-container">
-            <img src="LogoLP.png" alt="LP Logo" />
+            <img src="images/LogoLP.png" alt="LP Logo" />
           </div>
 
-          <!-- Main Search -->
+
           <div class="main-search">
             <div class="main-search-bar">
-              <div class="search-icon">⌘</div>
               Rechercher avec google ou autre
             </div>
           </div>
 
-          <!-- Apps -->
+
           <div class="apps-grid">
             <div class="app">
               <img src="https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg" alt="Photoshop">
@@ -61,6 +38,18 @@
 
             <div class="app">
               <img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg" alt="VS Code">
+            </div>
+            <div class="app">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Php-logo.png/960px-Php-logo.png?utm_source=commons.wikimedia.org&utm_campaign=imageinfo&utm_content=thumbnaill" alt="VS Code">
+            </div>
+            <div class="app">
+                <img src="images/Procreate.jpeg" alt="Procreate">
+            </div>
+            <div class="app">
+                <img src="images/Procreate-Dreams.jpeg" alt="Procreate">
+            </div>
+            <div class="app">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original" alt="VS Code">
             </div>
         </div>
 </body>
